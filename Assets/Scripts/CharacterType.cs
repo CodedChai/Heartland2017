d0709866d31b6d6d225a933e82d3efdfff53114c
Alvaro Gudiswitz-Starred Repositories are significant projects <alvarogudis@gmail.com>
2017-04-01 11:25:29 -0500

    Basic Movement

---
+++ Assets/Scripts/CharacterType.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterType : MonoBehaviour {
+
+	protected int hp =0;
+	protected float movespeed=0;
+
+	//gets the max HP. Current HP is stored in the player controller.
+	public int GetHP()
+	{
+		return hp;
+	}
+
+	public float GetMoveSpeed()
+	{
+		return movespeed;
+	}
+	public void Primary(float joyX, float joyY)
+	{ }
+
+	public void Secondary()
+	{	}
+
+}
---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : CharacterType {
+
+	// Use this for initialization
+	void Start () {
+		//overload movespeed and hp here.
+		movespeed = 3;
+		hp = 4;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
