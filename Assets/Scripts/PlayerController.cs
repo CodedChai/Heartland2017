065f736d909769cad28ae966ef252faf193b417b
alvarpq <alvarogudis@gmail.com>
2017-04-01 13:42:45 -0400

    Added prefabs I think? Still a bit confused with how the structure works, so let's hope I didn't fuck anything up[.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-
+    public CharacterType character;
+    float speed;
+    int maxHP;
 	// Use this for initialization
 	void Start () {
-		
+        speed = character.GetMoveSpeed();
+        maxHP = character.GetHP();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        transform.Translate(new Vector3(speed * Input.GetAxis("Horizontal") * Time.deltaTime, speed * Input.GetAxis("Vertical") * Time.deltaTime));
+    }
 }
