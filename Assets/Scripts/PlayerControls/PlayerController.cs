9a170f5297b8b6dee2ce883069e0ff687516033c
alvarpq <alvarogudis@gmail.com>
2017-04-08 04:01:42 -0400

    quick fix

--- Assets/Scripts/PlayerControls/PlayerController.cs
+++ Assets/Scripts/PlayerControls/PlayerController.cs
@@ -86,6 +86,11 @@ public class PlayerController : MonoBehaviour {
                 np.name = name;
                 np.name.text = np.GetComponent<CharacterType>().name;
                 characterType.dead = true;//it should kill itself
+
+                cam.transform.SetParent(og.transform);
+                cam.transform.localPosition = new Vector3(0, 0, -10);
+
+
                 Destroy(this);
             }
             else
